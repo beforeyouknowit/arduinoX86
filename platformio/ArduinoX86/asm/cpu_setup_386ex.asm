017f54bcc25dcd228f0c7b284a3de72fad17f451
dbalsom <dbalsom@gmail.com>
2025-08-17 09:11:37 -0400

    commit 386ex setup program source

---
+++ platformio/ArduinoX86/asm/cpu_setup_386ex.asm
@@ -0,0 +1,21 @@
+; cpu_setup_386ex.asm
+cpu	386
+org	0h
+
+start:
+    jmp   -100h
+    times 8 nop
+    mov   ax, 08000H    ; Enable expanded I/O space
+    out   23H, al       ; unlock the re-map bits
+    xchg  al, ah
+    out   22H, al       ; Knock on the port
+    out   22H, ax       ; Knock again with 16-bits to set
+    mov   dx, 0F438h
+    mov   ax, 0380h
+    out   dx, ax
+    mov   dx, 0F43Eh
+    mov   ax, 0
+    out   dx, ax
+    mov   ax, 1
+    mov   dx, 0F43Ch
+    out   dx, ax
