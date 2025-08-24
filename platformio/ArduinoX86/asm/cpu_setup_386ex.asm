8644f0378e74768731d7d7c1f07ce7a08814e855
dbalsom <dbalsom@gmail.com>
2025-08-24 10:27:31 -0400

    comments

--- platformio/ArduinoX86/asm/cpu_setup_386ex.asm
+++ platformio/ArduinoX86/asm/cpu_setup_386ex.asm
@@ -10,12 +10,12 @@ start:
     xchg  al, ah
     out   22H, al       ; Knock on the port
     out   22H, ax       ; Knock again with 16-bits to set
-    mov   dx, 0F438h
+    mov   dx, 0F438h    ; UCSADL - Chip-select Low Address
     mov   ax, 0380h
     out   dx, ax
-    mov   dx, 0F43Eh
+    mov   dx, 0F43Eh    ; UCSMSKH - Chip-select High Mask
     mov   ax, 0
     out   dx, ax
     mov   ax, 1
-    mov   dx, 0F43Ch
+    mov   dx, 0F43Ch    ; UCSMSKL - Chip-select Low Mask
     out   dx, ax
